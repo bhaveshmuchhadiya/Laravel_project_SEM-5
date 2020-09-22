94f9990fb17699702c0f5fcc1bba0f47e4686116
bhavesh muchhadiya <bhaveshmuchhadiya1999.com>
2020-09-22 12:43:42 +0530

    header footer maintain

---
+++ resources/views/index.blade.php
@@ -0,0 +1,42 @@
+		<!-- main content start-->
+		<div id="page-wrapper">
+			<div class="main-page">
+			<div class="col_3">
+        	<div class="col-md-3 widget widget1">
+        		<div class="r3_counter_box">
+                    <i class="pull-left fa fa-pie-chart dollar1 icon-rounded"></i>
+                    <div class="stats">
+                      <h5><strong>50</strong></h5>
+                      <span>Leaves</span>
+                    </div>
+                </div>
+        	 </div>
+        	<!-- <div class="col-md-3 widget">
+        		<div class="r3_counter_box">
+                    <i class="pull-left fa fa-users dollar2 icon-rounded"></i>
+                    <div class="stats">
+                      <h5><strong>150</strong></h5>
+                      <span>Total Users</span>
+                    </div>
+                </div>
+        	 </div> -->
+        	<div class="clearfix"> </div>
+		</div>
+		
+		<div class="row-one widgettable">
+			
+			<div class="clearfix"> </div>
+		</div>
+				
+				
+	
+    <!--footer-->
+	<div class="footer">
+	   <p>&copy; 2018 Glance Design Dashboard. All Rights Reserved | Design by <a href="https://w3layouts.com/" target="_blank">w3layouts</a></p>		
+	</div>
+    <!--//footer-->
+	</div>
+		
+	
+</body>
+</html>
