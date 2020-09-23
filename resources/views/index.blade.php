fb8a4b2f29c19c27b01489101190d4a71f5ae9a5
bhavesh muchhadiya <bhaveshmuchhadiya1999.com>
2020-09-23 14:02:44 +0530

    cheaking repository

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
 		<div id="page-wrapper">
 			<div class="main-page">
 			<div class="col_3">
-        	<div class="col-md-3 widget widget1">
+        	<!-- <div class="col-md-3 widget widget1">
         		<div class="r3_counter_box">
                     <i class="pull-left fa fa-pie-chart dollar1 icon-rounded"></i>
                     <div class="stats">
@@ -12,7 +12,7 @@
                       <span>Leaves</span>
                     </div>
                 </div>
-        	 </div>
+        	 </div> -->
         	<div class="col-md-3 widget">
         		<div class="r3_counter_box">
                     <i class="pull-left fa fa-users dollar2 icon-rounded"></i>
